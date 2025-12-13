489c651d661c7c082bd3b6278d91b59e154a6094
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-13 09:18:03 +0000

    conversion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLogedIn = 1
+// let booleanisLogedIn = Boolean(isLogedIn)   
+// console.log(booleanisLogedIn)
+
+let someNumber = 58
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
