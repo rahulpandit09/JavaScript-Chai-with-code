f0ea7f65be6ae83bacb503580c05df9d6658237c
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-13 08:23:58 +0000

    Refactor account information logging and update console output format

--- first/index.js
+++ first/index.js
@@ -1,1 +1,15 @@
-console.log("Rahul Kumar")
+// const accountId = 1245
+// let accountEmail = "rahul.kumar@gmail.com"
+// var accountPassword = "12458"
+// accountCity = "bihar"
+// let accountState;
+
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+const Id = 45
+let name = "rahul"
+let phoneNumber = "457826825"
+let cityName = "bihar"
+
+console.table([Id, name, phoneNumber, cityName])
