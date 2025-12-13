bd294d93f8709667d6f22533dafc0c5508908a33
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-13 14:56:08 +0000

    practice typeconverction

---
+++ 01_Basics/index.js
@@ -0,0 +1,18 @@
+console.log("Hii Rahul How are you all good");
+
+// const Id = 45
+// let name = "Rahul"
+// name = "pavan"
+// console.log(name);
+
+// const employeeId = 78
+// let employeeName = "Rahul"
+// let employeePhoneNumber = "46532646946"
+// let employeeEmail = "rahul@gmail.com"
+
+// console.table([employeeId, employeeName, employeePhoneNumber, employeeEmail]);
+
+let number = 45
+
+let myNumber = String(Number)
+console.log(typeof (myNumber))
