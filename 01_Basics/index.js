cfed3cc77ee15454ef5e97f4d5ea94434a8b2e66
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-14 16:03:54 +0000

    all const and let

--- 01_Basics/index.js
+++ 01_Basics/index.js
@@ -1,4 +1,6 @@
-console.log("Hii Rahul How are you all good");
+// console.log("Hii Rahul How are you all good");
+
+const { use } = require("react");
 
 // const Id = 45
 // let name = "Rahul"
@@ -27,11 +29,116 @@ console.log("Hii Rahul How are you all good");
 // let myNumber = String(Number)
 // console.log(typeof (myNumber))
 
-let cityName = undefined;
-console.log(typeof cityName)
+// let cityName = undefined;
+// console.log(typeof cityName)
 
-let i = 2;
-let n = String(i)
-console.log(n);
+// let i = 2;
+// let n = String(i)
+// console.log(n);
 
+
+// let name = "Rahul";
+// const id = 90
+// let village = "bagwara"
+
+// console.log("hello world!");
+// const Id = 23
+// let customerNumber = "45654494"
+// let customerName = "Rahul Kumar"
+
+// const name = "rahul";
+// console.log(name);
+
+// const age = 23;
+// const pi = 78.35;
+
+// console.table([age, pi]);
+
+// const user = {
+//     myName : "Rahul Kumar",
+//     age : 45,
+//     Id : 45,
+//     phoneNumber : 5777845444
+// };
+
+// console.table([user])
+
+
+// const fruits = ["Apple","Mango","Banana"];
+// fruits.push("Kela and papaya")
+// console.table([fruits]);
+
+
+// for(const number of [10, 20, 34, 23]){
+//     console.log(number);
+// }
+
+// const komal = function (){
+//     console.log("hi this is rahul and I'm learning JavaScript const variable"); 
+// }
+//  komal();
+ 
+// const add = (a, b) => (a + b);
+// console.log(add(10, 20));
+
+// let name = "Rahul";
+// console.log(name)
+
+// myName = "Amit Kumar"
+// console.log(myName);
+
+// let score = 70;
+// score = 79;
+// score = score + 45;
+
+// console.log(score);
+
+// if(true){
+//     let massage = "Hi rahul How are You Everything Good"
+//     console.log(massage)
+// }
+
+// for(let i =0; i<=10; i++){
+//     console.table([i])
+// }
+
+
+// for(let j=1; j<9; j++){
+//     console.table([j]);
+// }
+
+// let color = ["red", "tellow"];
+// color.push("purple");
+// color = ["green","black","white"]
+// console.table([color])
+
+
+// let user = {
+//     myName : "Rahul",
+//     id : 46,
+//     phone : 346464,
+//     email : "rahulpandot@gmail.com"
+// };
+
+// console.log(user);
+
+// let Login = {
+//     userName : "rahul123",
+//     Password : 45565
+// };
+
+// console.log(Login);
+
+// let user = {
+//   name: "Rahul",
+//   age: 24
+// };
+
+// user.age = 25;       
+// user = { name: "Amit", age: 30 };
+
+// console.log(user);
+
+let myName  = "paban";
+console.log(myName);
 
