cff7a0aba716da1516d6d3649ea0f78f985af031
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-15 14:25:44 +0000

    first practice code

---
+++ 01_Basics/first.js
@@ -0,0 +1,18 @@
+// console.log("Rahul")
+
+// let Login = {
+//     userName : "rahul123",
+//     password : "rahul@123"
+// };
+
+// console.log(Login);
+
+
+let Customer = {
+    customerName : "Rahul Pandit",
+    customerId : 47,
+    customerEmail : "rahulpandit@gmail.com",
+    customerPhoneNumber : "548787487"
+};
+
+console.table([Customer]);
