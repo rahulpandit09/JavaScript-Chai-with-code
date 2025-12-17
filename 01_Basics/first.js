36e1786761f3ed8cb7fd93d5c976163399007008
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-17 16:01:26 +0000

    all packages with datatypes practice in first.js file

--- 01_Basics/first.js
+++ 01_Basics/first.js
@@ -1,5 +1,7 @@
 // console.log("Rahul")
 
+const { use } = require("react");
+
 // let Login = {
 //     userName : "rahul123",
 //     password : "rahul@123"
@@ -8,11 +10,85 @@
 // console.log(Login);
 
 
-let Customer = {
-    customerName : "Rahul Pandit",
-    customerId : 47,
-    customerEmail : "rahulpandit@gmail.com",
-    customerPhoneNumber : "548787487"
+// let Customer = {
+//     customerName : "Rahul Pandit",
+//     customerId : 47,
+//     customerEmail : "rahulpandit@gmail.com",
+//     customerPhoneNumber : "548787487"
+// };
+
+// console.table([Customer]);
+
+//Today I Will practice Data type in javaScript
+//Primitive datatype 
+
+let city = "bihar";
+let course = "javascript";
+
+console.log(city);
+console.log(typeof course);
+
+let age = 34;
+let price = 78.5;
+
+console.log(age);
+console.log(typeof price);
+
+let isLoggedIn = true;
+let isAdmin = false;
+
+console.log(isLoggedIn);
+console.log(typeof isAdmin);
+
+let result;
+
+console.log(result);
+console.log(typeof result);
+
+let data = null;
+
+console.log(data);
+console.log(typeof data);
+
+let bigNumber = 6497646413684897649879764n
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+//Non Primitive Data Types
+let usere = {
+    Name : "Rahul",
+    age : 45,
+    isStudent : true
 };
 
-console.table([Customer]);
+console.log(usere);
+
+let users = {
+  name: "Rahul",
+  age: 24,
+  isStudent: true
+};
+
+console.log(users);
+console.log(typeof users);
+
+let fruits = ['apple','mango','banana'];
+
+console.log(fruits);
+console.log(typeof fruits);
+
+let user = {
+    name : "rahul",
+    phone : 248973659,
+    isUser : true
+}
+
+console.log(user);
+console.log(typeof user);
+
+function colore(){
+    console.log("This is a colore list");   
+}
+
+console.log(colore());
+console.log(typeof colore);
