47352f4d6269a554ff7537f19abfa395f23737b4
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-17 16:42:17 +0000

    changing the name

---
+++ 01_Basics/07_stackhep.js
@@ -0,0 +1,22 @@
+//stack(Primitive)         Hep - (Non-primitive)
+//<======================stack=======================>
+let myName = "Rahul Kumar";
+let anotherName = myName
+anotherName = "Rahul Pandit";
+
+console.log(myName);
+console.log(anotherName);
+
+//<=======================Hep===============================>
+let oneNeme = {
+    email : "rahulGoogle@gamil.com",
+    id : 54946987,
+};
+
+let oneAnotherName = oneNeme
+oneAnotherName.email = "rahulkashyap.kumar@gmail.com"
+
+console.log(oneNeme.email);
+console.log(oneAnotherName.email);
+
+console.log("Hello world!");
