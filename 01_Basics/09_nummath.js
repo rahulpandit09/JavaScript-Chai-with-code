50c96cd5bb379912f55cc80d730005ae57418b0b
Rahul kumar <rahul.kashyaprp09@gmail.com>
2025-12-19 16:51:33 +0000

    Maths and Numar

---
+++ 01_Basics/09_nummath.js
@@ -0,0 +1,37 @@
+// const score = 400
+// console.log(score);
+
+// let myscore = new Number(100);
+// console.log(myscore);
+
+// console.log(myscore.toString().length);
+// console.log(myscore.toFixed(2));
+
+// const otherNumbers = 1234.359
+// console.log(otherNumbers.toPrecision(5));
+
+// const hunderade = 1000000
+// console.log(hunderade.toLocaleString('en-IN'));
+
+// <=========================Math===============================>
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(9.45));
+// console.log(Math.max(4, 5, 5, 6, 8));
+// console.log(Math.min(7, 1 ,3, 4, 5));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 20
+const max = 40
+
+
+//This is the most important formula most of the time we are using this code. 
+//so tomorror when you come again you watch 1.5 speed and alos practice becouse this is the most important concepts .
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
