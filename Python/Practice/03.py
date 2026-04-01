02d488bc0175ee7fb7ff5121dce16a4b39d1027e
Rahul kumar <rahul.kashyaprp09@gmail.com>
2026-04-01 12:35:46 +0000

    for loop

---
+++ Python/Practice/03.py
@@ -0,0 +1,34 @@
+# n = int(input("Enter a number : "))
+
+# for i in range(1, n+1):
+#     print(" " * (n-i), end="")
+#     print("*" * (2*i-1))
+
+
+# n = int(input("Enter a number : "))
+
+# for i in range(1, n+1):
+#     print(" "* (n-1) + "*" (2*i-1))
+
+# n = int(input("Enter a number : "))
+# for i in range(1, n+1):
+#     print("*"*i)
+
+
+# n = int(input("Enter a number"))
+# for i in range(1, n+1):
+#     print("*" * i)
+
+
+# n = 4;
+# for i in range(1, n+1):
+#     print("*"* (n-i+1))
+
+
+# n = 5
+# for i in range(1, n+1):
+#     print(" "*(n-i)+"*" *(2*i-1))
+
+n = 5
+for i in range(1, n+1):
+    print("*"*(n-i)+"*"(2*i-1))
