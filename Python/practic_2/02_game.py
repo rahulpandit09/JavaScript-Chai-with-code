7fe3a260addcab2436907f64b882686c6019cc9d
Rahul kumar <rahul.kashyaprp09@gmail.com>
2026-04-16 08:19:09 +0000

    changes code

--- Python/practic_2/02_game.py
+++ Python/practic_2/02_game.py
@@ -2,7 +2,7 @@ import random
 
 def game():
     print("You are playing game...")
-    score = random.randint(1, 63)
+    score = random.randint(1, 100)
     # featch the hiscore
     with open("/workspaces/JavaScript-Chai-with-code/Python/practic_2/hiscore.txt") as f:
         hiscore = f.read()
@@ -13,6 +13,7 @@ def game():
         
     print(f"Your score: {score}")
     if(score>hiscore):
+        # write the hiscore to this file
         with open("/workspaces/JavaScript-Chai-with-code/Python/practic_2/hiscore.txt", "w") as f:
             f.write(str(score))
     return score
